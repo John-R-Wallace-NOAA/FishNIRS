caa28b94679c66b3c38b450bcab4dd0bfad2d748
John Wallace <John.Wallace@noaa.gov>
2021-01-21 02:29:53 -0800

    Update readSpectraData.R

--- R/readSpectraData.R
+++ R/readSpectraData.R
@@ -200,7 +200,9 @@ readSpectraData <- function(UploadDates, nearestColSubset = TRUE) {
                        stop("Good matching to metadata did not occur!")
                                        
                    Spc_Meta_df <- Spc_Meta_df[, c(1:2, N + 3:26, 3:(N + 2))]  # Move metadata to the front
+           print(names(Spc_Meta_df)[1:28])
                    metaDataNames <- c(names(Spc_df)[1:2], 'Storage', names(hakeMetaData)[-24]) # 26 metadata columns
+            print( metaDataNames  )     
                    print(Spc_Meta_df[1:4, c(metaDataNames, WaveFreqsUsed[1:2]) ])
                    hakeStabSpcStudy <- rbind(hakeStabSpcStudy, Spc_Meta_df)
                 }
