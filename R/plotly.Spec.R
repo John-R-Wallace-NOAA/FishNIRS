71a8a8f6caec820a94d60c46445f2058e4e209b8
John Wallace <John.Wallace@noaa.gov>
2022-11-03 18:22:17 -0700

    bgcolor, main, xlim, xylim args added

--- R/plotly.Spec.R
+++ R/plotly.Spec.R
@@ -1,5 +1,6 @@
-plotly.Spec <- function(spectraMeta, N_Samp = 50, randomAfterSampNum = NULL, colorGroup = 'Age', WaveRange = c(0, 8000), scanUniqueName = 'shortName', freqNum = NULL, xlab = "Wavenumber", 
-                           ylab = "Absorbance", plot = TRUE, reverse.plot.order = FALSE, verbose = FALSE) {
+plotly.Spec <- function(spectraMeta, N_Samp = 50, randomAfterSampNum = NULL, colorGroup = 'Age', WaveRange = c(0, 8000), scanUniqueName = 'shortName', 
+                  freqNum = NULL, xlab = "Wavenumber", ylab = "Absorbance", plot = TRUE, reverse.plot.order = FALSE, bgcolor = "#e5ecf6", 
+                  main = NULL, xlim = NULL, ylim = NULL, verbose = FALSE, ...) {
     
    require(plotly)
    renum <- function(x, no.num = F) {
@@ -19,12 +20,10 @@ plotly.Spec <- function(spectraMeta, N_Samp = 50, randomAfterSampNum = NULL, col
      showgrid = FALSE,
      mirror = TRUE
    )  
-   
-   xax <- a
-   yax <- a
-   xax$title <- xlab
-   yax$title <- ylab
-   
+  
+   xax <- list(title = xlab, range = xlim)
+   yax <- list(title = ylab, range = ylim)
+  
    if(reverse.plot.order)
       spectraMeta <- spectraMeta[nrow(spectraMeta):1, ]
   
@@ -42,7 +41,7 @@ plotly.Spec <- function(spectraMeta, N_Samp = 50, randomAfterSampNum = NULL, col
    
    spectraMeta <- spectraMeta[, c(scanUniqueName, WaveSubset, colorGroup)] 
    
-   if(is.null(N_Samp)) {
+   if(casefold(N_Samp) == "all"  ) {
        N_Samp <- nrow(spectraMeta)
        sampRows <- 1:N_Samp
        Spectra <- spectraMeta[, -c(1, ncol(spectraMeta))]
@@ -56,7 +55,7 @@ plotly.Spec <- function(spectraMeta, N_Samp = 50, randomAfterSampNum = NULL, col
       Spectra <- spectraMeta[sampRows, -c(1, ncol(spectraMeta))]      
    }
    
-   Spec <- renum(data.frame(as.matrix(data.frame(Scan = rep(spectraMeta[sampRows, scanUniqueName], each = freqNum.Subset),  # Double data.frame() is needed
+   Spec <- renum(data.frame(as.matrix(data.frame(Scan = rep(spectraMeta[sampRows, scanUniqueName], each = freqNum.Subset),  # Double use of  data.frame() is needed
                  Band = rep(as.numeric(names(Spectra)), N_Samp), Value = c(as.matrix(t(Spectra))), 
                  Color = rep(spectraMeta[sampRows, grep(colorGroup, names(spectraMeta))[1]], each = freqNum.Subset))))) 
    Spec$Band <- as.numeric(Spec$Band)
@@ -73,8 +72,8 @@ plotly.Spec <- function(spectraMeta, N_Samp = 50, randomAfterSampNum = NULL, col
                    hoverinfo = 'text',
                    text = ~paste('Scan: ', Scan,
                                  '</br></br> x-axis: ', Band,
-                                 '</br> y-axis: ', Value)) %>%
-          layout(xaxis = xax, yaxis = yax) %>%
+                                 '</br> y-axis: ', Value), ...) %>%
+          layout(title = main, plot_bgcolor = bgcolor, xaxis = xax, yaxis = yax) %>%
           add_lines(line = list(width = 0.75)))
   }     
        
