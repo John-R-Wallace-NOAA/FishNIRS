22220b1f4d0bbaa7bca185a63d44fc740cd33f15
John Wallace <John.Wallace@noaa.gov>
2022-11-04 20:21:00 -0700

    ggplot2 facet added for another dimension

--- R/plotly.Spec.R
+++ R/plotly.Spec.R
@@ -1,6 +1,6 @@
-plotly.Spec <- function(spectraMeta, N_Samp = 50, randomAfterSampNum = NULL, colorGroup = 'Age', WaveRange = c(0, 8000), scanUniqueName = 'shortName', 
-                  freqNum = NULL, xlab = "Wavenumber", ylab = "Absorbance", plot = TRUE, reverse.plot.order = FALSE, bgcolor = "#e5ecf6", 
-                  main = NULL, xlim = NULL, ylim = NULL, verbose = FALSE, ...) {
+plotly.Spec <- function(spectraMeta, N_Samp = 50, randomAfterSampNum = NULL, colorGroup = 'Age', facetGroup = 'scanGroup', WaveRange = c(0, 8000), 
+                  scanUniqueName = 'shortName', freqNum = NULL, xlab = "Wavenumber", ylab = "Absorbance", plot = TRUE, reverse.plot.order = FALSE, 
+                  bgcolor = "#e5ecf6", main = NULL, xlim = NULL, ylim = NULL, verbose = FALSE, ...) {
     
    require(plotly)
    renum <- function(x, no.num = F) {
@@ -38,49 +38,108 @@ plotly.Spec <- function(spectraMeta, N_Samp = 50, randomAfterSampNum = NULL, col
    WaveLengths <- as.numeric(names(spectraMeta[, 2:(freqNum + 1)]))
    WaveSubset <- as.character(WaveLengths[WaveLengths >= WaveRange[1] & WaveLengths <= WaveRange[2]])
    freqNum.Subset <- length(WaveSubset)
+   if(verbose)
+         cat("\nNumber of Frequencies subset by the wave range (WaveRange) = ", freqNum.Subset, "\n")   
    
-   spectraMeta <- spectraMeta[, c(scanUniqueName, WaveSubset, colorGroup)] 
+   if(is.null(facetGroup)) {
    
-   if(casefold(N_Samp) == "all"  ) {
-       N_Samp <- nrow(spectraMeta)
-       sampRows <- 1:N_Samp
-       Spectra <- spectraMeta[, -c(1, ncol(spectraMeta))]
-   } else {
-      if(is.null(randomAfterSampNum)) {
-         sampRows <- sample(1:nrow(spectraMeta), N_Samp)
-      } else {
-         sampRows <- c(1:randomAfterSampNum, sample((randomAfterSampNum + 1):nrow(spectraMeta), N_Samp))
-         N_Samp <- length(1:randomAfterSampNum) + N_Samp
-      }   
-      Spectra <- spectraMeta[sampRows, -c(1, ncol(spectraMeta))]      
-   }
-   
-   Spec <- renum(data.frame(as.matrix(data.frame(Scan = rep(spectraMeta[sampRows, scanUniqueName], each = freqNum.Subset),  # Double use of  data.frame() is needed
-                 Band = rep(as.numeric(names(Spectra)), N_Samp), Value = c(as.matrix(t(Spectra))), 
-                 Color = rep(spectraMeta[sampRows, grep(colorGroup, names(spectraMeta))[1]], each = freqNum.Subset))))) 
-   Spec$Band <- as.numeric(Spec$Band)
-   Spec$Value <- as.numeric(Spec$Value)
+      spectraMeta <- spectraMeta[, c(scanUniqueName, WaveSubset, colorGroup)] 
       
-   if(verbose) {
-      print(str(Spec))
-      print(Spec[1:4,])                
-   }   
-   if(plot) {
-      print(plot_ly(Spec, 
-                   x = ~Band, y = ~Value, split = ~Scan, color = ~Color, 
-                   colors = rainbow(length(unique(Spec$Color))), 
-                   hoverinfo = 'text',
-                   text = ~paste('Scan: ', Scan,
-                                 '</br></br> x-axis: ', Band,
-                                 '</br> y-axis: ', Value), ...) %>%
-          layout(title = main, plot_bgcolor = bgcolor, xaxis = xax, yaxis = yax) %>%
-          add_lines(line = list(width = 0.75)))
-  }     
-       
-  if(sum(is.na(as.numeric(Spec$Color))) < 0.20 * length(Spec$Color))
-       Spec$Color <- as.numeric(Spec$Color)    
-       
-  names(Spec)[4] <- colorGroup 
-
+      if(casefold(N_Samp) == "all"  ) {
+          N_Samp <- nrow(spectraMeta)
+          sampRows <- 1:N_Samp
+          Spectra <- spectraMeta[, -c(1, ncol(spectraMeta))]
+      } else {
+         if(is.null(randomAfterSampNum)) {
+            sampRows <- sample(1:nrow(spectraMeta), N_Samp)
+         } else {
+            sampRows <- c(1:randomAfterSampNum, sample((randomAfterSampNum + 1):nrow(spectraMeta), N_Samp))
+            N_Samp <- length(1:randomAfterSampNum) + N_Samp
+         }   
+         Spectra <- spectraMeta[sampRows, -c(1, ncol(spectraMeta))]      
+      }
+      
+      Spec <- renum(data.frame(as.matrix(data.frame(Scan = rep(spectraMeta[sampRows, scanUniqueName], each = freqNum.Subset),  # Double use of  data.frame() is needed
+                    Band = rep(as.numeric(names(Spectra)), N_Samp), Value = c(as.matrix(t(Spectra))), 
+                    Color = rep(spectraMeta[sampRows, grep(colorGroup, names(spectraMeta))[1]], each = freqNum.Subset)
+                    )))) 
+      Spec$Band <- as.numeric(Spec$Band)
+      Spec$Value <- as.numeric(Spec$Value)
+         
+      if(verbose) {
+         print(str(Spec))
+         print(Spec[1:4,])                
+      }   
+      if(plot) {
+         print(plot_ly(Spec, 
+                      x = ~Band, y = ~Value, split = ~Scan, color = ~Color, 
+                      colors = rainbow(length(unique(Spec$Color))), 
+                      hoverinfo = 'text',
+                      text = ~paste('Scan: ', Scan,
+                                    '</br></br> x-axis: ', Band,
+                                    '</br> y-axis: ', Value), ...) %>%
+             layout(title = main, plot_bgcolor = bgcolor, xaxis = xax, yaxis = yax) %>%
+             add_lines(line = list(width = 0.75)))
+      }     
+          
+      if(sum(is.na(as.numeric(Spec$Color))) < 0.20 * length(Spec$Color))
+          Spec$Color <- as.numeric(Spec$Color)    
+          
+      names(Spec)[4] <- colorGroup
+      
+   } else {
+   
+      spectraMeta <- spectraMeta[, c(scanUniqueName, WaveSubset, colorGroup, facetGroup)] 
+      
+      if(casefold(N_Samp) == "all"  ) {
+          N_Samp <- nrow(spectraMeta)
+          sampRows <- 1:N_Samp
+          Spectra <- spectraMeta[, -c(1, ncol(spectraMeta) - 1, ncol(spectraMeta))]
+      } else {
+         if(is.null(randomAfterSampNum)) {
+            sampRows <- sample(1:nrow(spectraMeta), N_Samp)
+         } else {
+            sampRows <- c(1:randomAfterSampNum, sample((randomAfterSampNum + 1):nrow(spectraMeta), N_Samp))
+            N_Samp <- length(1:randomAfterSampNum) + N_Samp
+         }   
+         Spectra <- spectraMeta[sampRows, -c(1, ncol(spectraMeta) - 1, ncol(spectraMeta))]      
+      }
+      
+      Spec <- renum(data.frame(as.matrix(data.frame(Scan = rep(spectraMeta[sampRows, scanUniqueName], each = freqNum.Subset),  # Double use of  data.frame() is needed
+                    Band = rep(as.numeric(names(Spectra)), N_Samp), Value = c(as.matrix(t(Spectra))), 
+                    Color = rep(spectraMeta[sampRows, grep(colorGroup, names(spectraMeta))[1]], each = freqNum.Subset),
+                    Facet = rep(spectraMeta[sampRows, grep(facetGroup, names(spectraMeta))[1]], each = freqNum.Subset)
+                    )))) 
+      Spec$Band <- as.numeric(Spec$Band)
+      Spec$Value <- as.numeric(Spec$Value)
+         
+      if(verbose) {
+         print(str(Spec))
+         print(Spec[1:4,])                
+      }   
+      # if(plot) {
+      #    print(plot_ly(Spec, 
+      #                 x = ~Band, y = ~Value, split = ~Scan, color = ~Color, 
+      #                 colors = rainbow(length(unique(Spec$Color))), 
+      #                 hoverinfo = 'text',
+      #                 text = ~paste('Scan: ', Scan,
+      #                               '</br></br> x-axis: ', Band,
+      #                               '</br> y-axis: ', Value), ...) %>%
+      #        layout(title = main, plot_bgcolor = bgcolor, xaxis = xax, yaxis = yax) %>%
+      #        add_lines(line = list(width = 0.75)))
+      # }     
+      if(plot) {
+          p <- ggplot(data = d_all, aes(x = Band, y = Value, z = Scan)) + geom_line(aes(colour = Color), size = 0.2) + facet_wrap(~ Facet)
+          ggplotly(p)
+      }
+          
+      if(sum(is.na(as.numeric(Spec$Color))) < 0.20 * length(Spec$Color))
+          Spec$Color <- as.numeric(Spec$Color)    
+          
+      names(Spec)[4] <- colorGroup 
+      names(Spec)[5] <- facetGroup   
+  
+   }
+  
   invisible(Spec)       
 }  
