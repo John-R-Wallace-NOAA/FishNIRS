af482272bf17e396632ef00b7ffab0d340b1ed6f
John Wallace <John.Wallace@noaa.gov>
2023-09-11 16:37:55 -0700

    Update Hake 2019, iPLS, NN.R

--- R_NN_Model_Hake_Scripts/Hake 2019, iPLS, NN.R
+++ R_NN_Model_Hake_Scripts/Hake 2019, iPLS, NN.R
@@ -102,7 +102,7 @@ tensorflow::set_random_seed(Seed_Model, disable_gpu = Disable_GPU)
 
 # --- Load and look at the raw spectra and metadata  ---
 {
-# See 'Read OPUS Hake 2019 Spectra into R.R' in this repo for how to read OPUS files in to R using Philipp Baumann's 'simplerspec' R package.
+# See 'Read OPUS Hake 2019 Spectra into R.R' in this repo for how to read OPUS files into R using Pierre Roudier's 'opusreader' R package.
 load("hake_all_2019.8.10 ORG.RData") 
 
 # Look at the data and metadata
