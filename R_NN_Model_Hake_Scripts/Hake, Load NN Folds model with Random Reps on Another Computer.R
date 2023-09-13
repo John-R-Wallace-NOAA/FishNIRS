7478618eb62c7f3aaac67effc8343163e881c563
John Wallace <John.Wallace@noaa.gov>
2023-09-12 19:14:19 -0700

    Update Hake, Load NN Folds model with Random Reps on Another Computer.R

--- R_NN_Model_Hake_Scripts/Hake, Load NN Folds model with Random Reps on Another Computer.R
+++ R_NN_Model_Hake_Scripts/Hake, Load NN Folds model with Random Reps on Another Computer.R
@@ -1,11 +1,44 @@
 
-# --- Initial setup  (The curly brakets without  indented lines directly below are there to enable the  hiding of code sections using Notepad++.) ---
-{
-setwd("C:/ALL_USR/JRW/SIDT/Hake Data 2019") # Change this path as needed.
+# Change the path TensorFlow as needed
+Sys.setenv("RETICULATE_PYTHON" = "C:/Users/John.Wallace/AppData/Local/miniconda3/envs/tf")
+Sys.getenv("RETICULATE_PYTHON")
+
+# Get the remotes package, if it is not already installed.
+if (!any(installed.packages()[, 1] %in% "remotes"))  install.packages('remotes')  
+
+remotes::install_github("John-R-Wallace-NOAA/JRWToolBox", INSTALL_opts = "--no-staged-install")
+library(JRWToolBox)
+
+lib(tensorflow)
+lib(keras)
+k_clear_session()
+
+# Test TensorFlow environment
+a <- tf$Variable(5.56)
+b <- tf$Variable(2.7)
+a + b
+
+Seed_Data <- c(777, 747, 727, 787, 797)[3]
+set.seed(Seed_Data)
+
+Seed_Model <- c(777, 747, 727, 787, 797)[3]
+tensorflow::set_random_seed(Seed_Model,  disable_gpu = c(TRUE, FALSE)[1]) 
+
+lib(reticulate)
+lib(tidyverse)
+lib(recipes)
+lib(rsample)
+lib(GGally)
+lib(skimr)
+lib(e1071)
+lib(plotly)
+
+# lib(openxlsx)
+
 
 sourceFunctionURL <- function (URL,  type = c("function", "script")[1]) {
        " # For more functionality, see gitAFile() in the rgit package ( https://github.com/John-R-Wallace-NOAA/rgit ) which includes gitPush() and git() "
-       if (!any(installed.packages()[, 1] %in% "httr"))  install.packages("httr") 
+       require(httr)
        File.ASCII <- tempfile()
        if(type == "function")
          on.exit(file.remove(File.ASCII))
@@ -22,73 +55,23 @@ sourceFunctionURL <- function (URL,  type = c("function", "script")[1]) {
        }  
 }
 
-# Toolbox functions 
-if (!any(installed.packages()[, 1] %in% "JRWToolBox"))  {
-    sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/openwd.R")
-    sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/lib.R")
-    sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/get.subs.R")
-    sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/sort.f.R")
-    sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/predicted_observed_plot.R")
-    sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/residuals_plot.R")
-    sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/as.num.R")
-    sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/match.f.R")
-    sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/Table.R")
-    sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/renum.R")
-    sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/agg.table.R")
-    sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/r.R")
-    sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/gof.R")
-    sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/Date.R")
-    sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/timeStamp.R")
-    sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/dec.R")
-    sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/loess.line.R")
-    sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/plot.loess.R")
-}    
-
-# FishNIRS funtions
+#FishNIRS funtion
 sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/FishNIRS/master/R/plotly.Spec.R")
 sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/FishNIRS/master/R/predicted_observed_plot.R")
 sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/FishNIRS/master/R/residuals_plot.R")
 sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/FishNIRS/master/R/Correlation_R_squared_RMSE_MAE_SAD.R")
 sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/FishNIRS/master/R/Mode.R")
 sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/FishNIRS/master/R/agreementFigure.R")
-sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/FishNIRS/master/R/FCNN_Model.R")
-sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/FishNIRS/master/R/CNN_model_ver_5.R")
-# sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/FishNIRS/master/R/CNN_model_2D.R")  # Not working yet
-
-
-lib(tidyverse)
-lib(recipes)
-lib(rsample)
-lib(GGally)
-lib(skimr)
-lib(e1071)
-lib(mdatools)
-lib(plotly)
-lib(reticulate)
-lib(tensorflow)
-lib(keras)
 
 
 
-# --- Setup for TensorFlow and Keras ---
-Sys.setenv("RETICULATE_PYTHON" = "C:/m/envs/tf_cpu_only") # Change this path to where your Conda TensorFlow environment is located.
-Sys.getenv("RETICULATE_PYTHON")
+setwd("W:/ALL_USR/JRW/SIDT/Hake Data 2019")
+base::load("W:\\ALL_USR\\JRW\\SIDT\\Hake Data 2019\\Hake_spectra_2019.sg.iPLS.RData")
+base::load("W:\\ALL_USR\\JRW\\SIDT\\Hake Data 2019\\Hake_TMA_2019.RData")
 
-# Test to see if  TensorFlow is working in R
-a <- tf$Variable(5.56)
-b <- tf$Variable(2.7)
-a + b
-
-k_clear_session() 
-
-}   
-
-# Load the raw spectra
-load("W:\\ALL_USR\\JRW\\SIDT\\Hake Data 2019\\Hake_spectra_2019.sg.iPLS.RData")
-load("W:\\ALL_USR\\JRW\\SIDT\\Hake Data 2019\\Hake_TMA_2019.RData")
-
-# Load saved model files (This could be intermediary results from a saved RData file of a model that is still running. )
-load('Hake_2019_Rdm_model_20_1_Apr_2023_09_26_40.RData')
+# Load saved models
+loadName <- 'Hake_2019_Rdm_models_1_Apr_2023_09_26_40' # Use the name from the saved model 
+base::load(paste0(loadName, '.RData'))
 
 
 # Split the data into folds, spitting the remainder of an un-even division into the first folds, one otie per fold until finished
@@ -110,6 +93,7 @@ lapply(folds_index, length)
 c(sum(unlist(lapply(folds_index, length))), length(index_org))  # Check that the number of oties is the same
 
 
+Delta <- -0.05 # Previous estimate or guess
 Rdm_reps <- length(Rdm_folds_index)
 y.fold.test.pred_RDM <- NULL
 for (j in 1:Rdm_reps) {
@@ -129,11 +113,10 @@ for (j in 1:Rdm_reps) {
    
    y.fold.test.pred_RDM <- rbind(y.fold.test.pred_RDM, y.test.pred)
    
-   dev.new(width = 11, height = 8)
-   agreementFigure(Hake_TMA_2019, y.test.pred, Delta = -0.05, full = TRUE, main = paste0("Random Rep = ", j)) # Delta is a previous estimate or guess for now
+   dev.new()
+   agreementFigure(Hake_TMA_2019, y.test.pred, Delta = -0.25, full = TRUE, main = paste0("Random Rep = ", j)) 
    
-   # Full figure only needed for a long-lived species like Sablefish
-   # dev.new(width = 11, height = 8)
+   # dev.new()
    # agreementFigure(Hake_TMA_2019, y.test.pred, Delta = -0.25, full = FALSE, main = paste0("Random Rep = ", j))
   
 }
@@ -158,9 +141,11 @@ for (Delta. in seq(0, -0.45, by  = -0.05)) {
       Delta Correlation   R_squared        RMSE         MAE         SAD 
    0.000000    0.959358    0.920368    0.772494    0.369007  862.000000 
 
+
  
-dev.new(width = 11, height = 8)
+dev.new()
 agreementFigure(Hake_TMA_2019, y.fold.test.pred_RDM_median, Delta = 0.0, full = TRUE, main = paste0("Median over ", Rdm_reps, ' Full k-Fold Models'), cex = 1.25)   
+
   
 
 # Apply that best Delta to all Rdm_reps models individually
@@ -177,7 +162,7 @@ Stats_RDM_median_by_model
    
 
 # An additional full k-fold added to the total number of models at each step   
-dev.new(width = 11, height = 8)
+dev.new()
 par(mfrow = c(3,2))  
 Delta <- 0.0
 Stats_RDM_median_by_model_added <- NULL
@@ -218,4 +203,14 @@ for (i in 1:5) {
    matplot(1:Rdm_reps, Stats_0_1_interval, type = 'o', col = c(1:3,6), xlab = 'Number of Complete Folds', ylab = 'Various Stats', main = 'Randomized Order')
 }
  
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
   
