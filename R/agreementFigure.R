b744276f105ef066a1f406393976bf678f513b03
John Wallace <John.Wallace@noaa.gov>
2023-12-04 17:18:03 -0800

    grey 1-to-1 line

--- R/agreementFigure.R
+++ R/agreementFigure.R
@@ -1,5 +1,5 @@
 
-agreementFigure <- function(Observed, Predicted, Delta = 0, Iter = 0, main = "", xlab = deparse(substitute(Observed)), 
+agreementFigure <- function(Observed, Predicted, Delta = 0, Iter = 0, main = "", xlab = deparse(substitute(Observed)), browserPlot = FALSE, 
                       ylab = paste0(deparse(substitute(Predicted)), " (rounded after adding Delta)"), full = FALSE, axes_zoomed_limits = 0:15, 
                       cex = ifelse(full, 0.75, 1.25), col_equal = 'red', col_off_1_or_2 = 'gold', col_off_3_or_4 = 'green', col_off_5_or_greater = 'navyblue', ...) {
   
@@ -25,6 +25,7 @@ agreementFigure <- function(Observed, Predicted, Delta = 0, Iter = 0, main = "",
    
    sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/renum.R")
    sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/match.f.R") 
+   sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/JRWToolBox/master/R/col.alpha.R") 
    sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/FishNIRS/master/R/Correlation_R_squared_RMSE_MAE_SAD.R")
    
    noNA <- na.omit(data.frame(Observed, Predicted))
@@ -52,16 +53,17 @@ agreementFigure <- function(Observed, Predicted, Delta = 0, Iter = 0, main = "",
    }   
    
    print(Correlation_R_squared_RMSE_MAE_SAD(Obs, Predicted.rd))
-   
+  
    plot(X, Y, main = main,
       xlab = paste0(xlab,': RMSE = ', signif(sqrt(mean((Predicted.rd - Obs)^2, na.rm = TRUE)), 6), '; SAD = ', 
                     signif(sum(abs(Predicted.rd - Obs)), 6), " (Prediction rounded after adding Delta for Stats)"), ylab = ylab, type = 'n', ...)
+                    
+   abline(0, 1, col = col.alpha('grey', ifelse(full, 0.50, 0.35)))
    
    text(Agreement_Table$Observed, Agreement_Table$Predicted, Agreement_Table$N_char, cex = cex, 
              col = ifelse(Agreement_Table$Observed == Agreement_Table$Predicted, 'red', 
                    ifelse(Agreement_Table$Observed == Agreement_Table$Predicted + 1 | Agreement_Table$Observed == Agreement_Table$Predicted - 1 |
                           Agreement_Table$Observed == Agreement_Table$Predicted + 2 | Agreement_Table$Observed == Agreement_Table$Predicted - 2, 'gold', 
                       ifelse(Agreement_Table$Observed == Agreement_Table$Predicted + 3 | Agreement_Table$Observed == Agreement_Table$Predicted - 3 |
-                             Agreement_Table$Observed == Agreement_Table$Predicted + 4 | Agreement_Table$Observed == Agreement_Table$Predicted - 4, 'green', 'navyblue'))))    
-     
+                             Agreement_Table$Observed == Agreement_Table$Predicted + 4 | Agreement_Table$Observed == Agreement_Table$Predicted - 4, 'green', 'navyblue'))))   
 }
