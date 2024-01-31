2ef9cad494674344fa2a1655ff0a3115829d94bf
John Wallace <John.Wallace@noaa.gov>
2024-01-31 15:29:29 -0800

    lots of fixes, some upgrades

--- R/Read_OPUS_Spectra.R
+++ R/Read_OPUS_Spectra.R
@@ -1,13 +1,16 @@
  
  
-# Sable_2020_Scans <- Read_OPUS_Spectra("Sable_2022", Spectra_Path = 'Sable_2022_Scans')
+# Sable_2020_Scans <- Read_OPUS_Spectra("Sable_Combo_2022", Spectra_Path = 'Sable_2022_Scans')
 # plotly_spectra(Sable_2020_Scans)
 # plotly_spectra(Sable_2020_Scans, N_Samp = 300, htmlPlotFolder = 'Figures/Sablefish_2022_Spectra_Sample_of_300')
 
 
- Read_OPUS_Spectra <- function(Spectra_Set = c("Hake_2019", "Sable_2017_2019", "Sable_2022")[3], Spectra_Path = NULL, verbose = c(TRUE, FALSE)[1], plot = c(TRUE, FALSE)[2],
-                         Max_N_Spectra = list(50, 200, 'All')[[3]], spectraInterp = c('stats_splinefun_lowess', 'prospectr_resample')[1], 
-                         opusReader = c('pierreroudier_opusreader', 'philippbaumann_opusreader2')[2], htmlPlotFolder = "Figures") { 
+ Read_OPUS_Spectra <- function(Spectra_Set = c("Hake_2019", "Sable_2017_2019", "Sable_Combo_2022")[3], Spectra_Path = NULL, 
+                         Meta_Add = c(TRUE, FALSE)[1], TMA_Ages = c(TRUE, FALSE)[2], Meta_Path = NULL, 
+                         excelSheet = 3, Max_N_Spectra = list(50, 200, 'All')[[3]], 
+                         verbose = c(TRUE, FALSE)[1], plot = c(TRUE, FALSE)[2], htmlPlotFolder = "Figures",
+                         spectraInterp = c('stats_splinefun_lowess', 'prospectr_resample')[1], 
+                         opusReader = c('pierreroudier_opusreader', 'philippbaumann_opusreader2')[2]) { 
  
  #  Max_N_Spectra is the max number of new spectra to be plotted in the spectra figure. 
  
@@ -35,9 +38,11 @@
     }  
    
    # (3) Sablefish 2022, Combo survey
-   if(Spectra_Set == "Sable_2022") { 
+   if(Spectra_Set == "Sable_Combo_2022") { 
       if(is.null(Spectra_Path)) 
-         Spectra_Path = 'C:/ALL_USR/JRW/SIDT/Sablefish 2022/Sable_2022_Scans'
+         Spectra_Path = 'C:/ALL_USR/JRW/SIDT/Sablefish 2022 Combo/Sable_2022_Scans'
+      if(Meta_Add & is.null(Meta_Path)) 
+         Meta_Path = paste0('C:/ALL_USR/JRW/SIDT/Sablefish 2022 Combo/', Spectra_Set, '_NIRS_Scanning_Session_Report.xlsx')
       shortNameSegments <- c(1,5) # Segments 1 and 3 of the spectra file name, e.g.: (SABLEFISH, COMBO201701203A, 28, OD1) => (SABLEFISH, 28)
       shortNameSuffix <- 'Combo'
       yearPosition <- c(6, 9) # e.g. COMBO201701203A => 2017 (Segment used (see above) is: shortNameSegments[1] + 1)
@@ -46,6 +51,24 @@
 
  # -----------------------------------------------------------------------------------------------------------------------------------
  
+   if(!any(installed.packages()[, 1] %in% "remotes")) 
+      install.packages("remotes") 
+
+   if(!any(installed.packages()[, 1] %in% "openxlsx")) 
+      install.packages("openxlsx")       
+    
+   if(!any(installed.packages()[, 1] %in% "dplyr")) 
+      install.packages("dplyr")     
+   
+   if(opusReader == 'pierreroudier_opusreader') {
+      if (!any(installed.packages()[, 1] %in% "opusreader")) 
+         remotes::install_github("pierreroudier/opusreader")   # https://github.com/pierreroudier/opusreader
+   }   
+   
+   if(opusReader == 'philippbaumann_opusreader2') {  
+      if (!any(installed.packages()[, 1] %in% "opusreader2")) 
+        remotes::install_github("spectral-cockpit/opusreader2")   #  https://github.com/spectral-cockpit/opusreader2
+   }
  
    sourceFunctionURL <- function (URL,  type = c("function", "script")[1]) {
           " # For more functionality, see gitAFile() in the rgit package ( https://github.com/John-R-Wallace-NOAA/rgit ) which includes gitPush() and git() "
@@ -77,34 +100,21 @@
    {
       ifelse(e1%%e2 == 0, e2, e1%%e2)
    }
-   
-   if (!any(installed.packages()[, 1] %in% "remotes")) 
-     install.packages("remotes") 
-   
-   if(opusReader == 'pierreroudier_opusreader') {
-      if (!any(installed.packages()[, 1] %in% "opusreader")) 
-         remotes::install_github("pierreroudier/opusreader")   # https://github.com/pierreroudier/opusreader
-   }   
-   
-   if(opusReader == 'philippbaumann_opusreader2') {  
-      if (!any(installed.packages()[, 1] %in% "opusreader2")) 
-        remotes::install_github("spectral-cockpit/opusreader2")   #  https://github.com/spectral-cockpit/opusreader2
-   }
-       
-  # --- The NN predicted ages will go in the path defined below ---
-  dir.create(htmlPlotFolder, showWarnings = FALSE)
      
 # -----------------------------------------------------------------------------------------------------------------------------------
- 
 
    # --- Create a character vector of all spectral files within 'Spectra_Path'---   
    if(is.null(Spectra_Path))
-      fileNames <- dir()
+      fileNames.0 <- dir()
    else   
-      fileNames <- dir(path = Spectra_Path)
-   cat(paste0("\nNumber of spectral files to be read in: ", length(fileNames), "\n\n"))
+      fileNames.0 <- dir(path = Spectra_Path)
+	  
+   if(verbose) {
+      cat(paste0("\nNumber of spectral files to be read in: ", length(fileNames.0), "\n\n"))
+      print(fileNames.0[1:10])
+   }
    
-   shortName <- apply(matrix(fileNames, ncol = 1), 1, function(x) paste(get.subs(x, sep = "_")[shortNameSegments], collapse = "_"))
+   shortName <- apply(matrix(fileNames.0, ncol = 1), 1, function(x) paste(get.subs(x, sep = "_")[shortNameSegments], collapse = "_"))
    if(!is.null(shortNameSuffix))
        shortName <- paste0(shortName, "_", shortNameSuffix)
       
@@ -113,7 +123,7 @@
    
        # ----------- Re-sampling wavebands using stats::splinefun() with lowess() (inside of the JRWToolBox::predict.lowess() function) or prospectr::resample() --------------
        newScans.ADJ <- list()
-       for (i in fileNames)  {
+       for (i in fileNames.0)  {
           print(i)
           if(opusReader == 'pierreroudier_opusreader')
              try(newScans.ADJ[[i]] <- opusreader::opus_read(paste(Spectra_Path, i , sep = "/"), simplify = FALSE, wns_digits = 0)[[2]] )
@@ -168,22 +178,56 @@
        print(head(newScans.RAW[, c(1:5, (ncol(newScans.RAW) - 25):(ncol(newScans.RAW) - 20))])); cat("\n\n")
    }
       
-  if(plot) {
-     sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/FishNIRS/master/R/plotly.Spec.R")
-     rowNums <- 1:nrow(newScans.RAW)
-     if(length(rowNums <= 26^2))
-        plotly.Spec(data.frame(filenames = fileNames, newScans.RAW, Otie = paste0(LETTERS[floor((rowNums - 0.001)/26) + 1], LETTERS[rowNums %r1% 26], '_', rowNums), shortName = shortName), N_Samp = min(c(length(fileNames), Max_N_Spectra)), colorGroup = 'Otie')
-     else
-       plotly.Spec(data.frame(filenames = fileNames, newScans.RAW, Otie = factor(rowNums), shortName = shortName), N_Samp = min(c(length(fileNames), Max_N_Spectra)), colorGroup = 'Otie') 
-  
-     if(!is.null(htmlPlotFolder))
-       saveHtmlFolder(htmlPlotFolder, view = !interactive())
+   if(plot) {
+      sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/FishNIRS/master/R/plotly.Spec.R")
+      rowNums <- 1:nrow(newScans.RAW)
+      if(length(rowNums <= 26^2))
+         plotly.Spec(data.frame(filenames = fileNames.0, newScans.RAW, Otie = paste0(LETTERS[floor((rowNums - 0.001)/26) + 1], LETTERS[rowNums %r1% 26], '_', rowNums), shortName = shortName), N_Samp = min(c(length(fileNames.0), Max_N_Spectra)), colorGroup = 'Otie')
+      else
+        plotly.Spec(data.frame(filenames = fileNames.0, newScans.RAW, Otie = factor(rowNums), shortName = shortName), N_Samp = min(c(length(fileNames.0), Max_N_Spectra)), colorGroup = 'Otie') 
+	  
+      if(!is.null(htmlPlotFolder)) {
+        dir.create(htmlPlotFolder, showWarnings = FALSE)
+        saveHtmlFolder(htmlPlotFolder, view = !interactive())
+      }  
    }    
-
 
    if(verbose)
       cat("\nDimension of Spectral File Matrix Read In:", dim(newScans.RAW), "\n\n")
-    
-   invisible(newScans.RAW)
+      
+   if(Meta_Add) {
+     fileNames <- get.subs(fileNames.0, sep = ".")[1, ]  # No '.0' in the metadata xlsx
+     metadata <- openxlsx::read.xlsx(Meta_Path, sheet = excelSheet) # Load in ancillary data 
+     Model_Spectra_Meta <- dplyr::left_join(data.frame(filenames = fileNames, newScans.RAW), metadata, dplyr::join_by("filenames" == "NWFSC_NIR_Filename")) # Match by filenames and look at the data/metadata
+     if(verbose)
+        print(Model_Spectra_Meta[1:5, c(1:3, (ncol(Model_Spectra_Meta) - 36):ncol(Model_Spectra_Meta))])
+     Model_Spectra_Meta$length_prop_max <- Model_Spectra_Meta$length_cm/max(Model_Spectra_Meta$length_cm, na.rm = TRUE)
+     Model_Spectra_Meta$structure_weight_dg = 10 * Model_Spectra_Meta$structure_weight_g # dg = decigram
+	 Model_Spectra_Meta$percent_crystallized_scan[is.na(Model_Spectra_Meta$percent_crystallized_scan)] <- 0 # Change NA to zero so that a numerical test can be done.
+	 Model_Spectra_Meta$percent_missing_scan[is.na(Model_Spectra_Meta$percent_missing_scan)] <- 0
+	 Model_Spectra_Meta$tissue_level_scan[is.na(Model_Spectra_Meta$tissue_level_scan)] <- 0
+     TF <- Model_Spectra_Meta$percent_crystallized_scan <= 15 & Model_Spectra_Meta$percent_crystallized_scan <= 10 & Model_Spectra_Meta$tissue_level_scan <= 10 & 
+                   !is.na(Model_Spectra_Meta$length_cm) & !is.na(Model_Spectra_Meta$structure_weight_g)
+	 names(Model_Spectra_Meta)[names(Model_Spectra_Meta) %in% 'age_best'] <- "TMA"		   
+     if(TMA_Ages)        
+         TF <- TF & !is.na(Model_Spectra_Meta$TMA)
+     if(verbose)
+        print(paste0('Total number of oties read in: ', sum(TF) + sum(!TF), '.  Number rejected based on metadata (including missing TMA, when asked for): ', sum(!TF), '.  Number kept: ', sum(TF), '.'), quote = FALSE)
+     Model_Spectra_Meta <- Model_Spectra_Meta[TF, ]    
+     invisible(Model_Spectra_Meta) 
+   } else  
+     invisible(data.frame(filenames = fileNames, newScans.RAW))
 }   
       
+	 
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
