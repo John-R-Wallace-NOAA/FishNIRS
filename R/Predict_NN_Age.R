04ae3aa1188344c1cbb40c695b4e04d376f43d4f
John Wallace <John.Wallace@noaa.gov>
2024-01-31 21:24:22 -0800

    Add files via upload

--- R/Predict_NN_Age.R
+++ R/Predict_NN_Age.R
@@ -43,8 +43,7 @@
 
 
 
-Predict_NN_Age <- function(Conda_TF_Eniv, Spectra_Path, Model_Spectra_Meta, NN_Model, plot = TRUE, htmlPlotFolder = NULL, NumRdmModels = NULL, shortNameSegments = c(2,4), shortNameSuffix = NULL, 
-     opusReader = c('pierreroudier_opusreader', 'philippbaumann_opusreader2')[2], spectraInterp = c('stats_splinefun_lowess', 'prospectr_resample')[1], fineFreqAdj = 150, Predicted_Ages_Path = NULL, verbose = FALSE,  ...) {
+Predict_NN_Age <- function(Conda_TF_Eniv, Spectra_Path, Model_Spectra_Meta, NN_Model, plot = TRUE, htmlPlotFolder = NULL, NumRdmModels = NULL, Predicted_Ages_Path = NULL, verbose = FALSE,  ...) {
    
    sourceFunctionURL <- function (URL,  type = c("function", "script")[1]) {
           " # For more functionality, see gitAFile() in the rgit package ( https://github.com/John-R-Wallace-NOAA/rgit ) which includes gitPush() and git() "
@@ -122,140 +121,29 @@ Predict_NN_Age <- function(Conda_TF_Eniv, Spectra_Path, Model_Spectra_Meta, NN_M
    
    # --- Load the NN model for SG_Variables_Selected and Rdm_models objects ---
    if(!verbose)
-     # base::load(NN_Model)
+      base::load(NN_Model)
       
    if(verbose) {
       '  ###  Just using envir = parent.frame() (the default) adds an environment it seems. Listing an envir arg to save locally is: base::load(file, envir = environment()), or use base::load(file) as done above ###  '
-     # base::load(NN_Model, envir = parent.frame()) # Save to [[.GlobalEnv]] 
+      base::load(NN_Model, envir = parent.frame()) # Save to [[.GlobalEnv]] 
       print(ll()); cat("\n\n")
       if(!interactive())  Sys.sleep(3)
    }
    
    # Find the metadata variables that are in the NN Model
    metaDataVar <- (1:length(SG_Variables_Selected))[is.na(as.numeric(substring(SG_Variables_Selected, 2)))]
-   SG_Scan_Variables_only <- SG_Variables_Selected[-metaDataVar]
-
-   
-   
-   
-   # --- Create a character vector of all spectral files within 'Spectra_Path'---   
-   (fileNames.0 <- dir(path = Spectra_Path))[1:10]
-   cat(paste0("\nNumber of spectral files to be read in: ", length(fileNames.0), "\n\n"))
-   fileNames <- get.subs(fileNames.0, sep = ".")[1, ]  # No '.0' suffix in the metadata xlsx
-   
-   
-   
-   shortName <- apply(matrix(fileNames, ncol = 1), 1, function(x) paste(get.subs(x, sep = "_")[shortNameSegments], collapse = "_"))
-   if(!is.null(shortNameSuffix))
-       shortName <- paste0(shortName, "_", shortNameSuffix)
-      
-      
-   # newScans.ADJ <- opusreader::opus_read(paste(Spectra_Path, i , sep = "/"), simplify = TRUE, wns_digits = 0)[[2]]
-      
-      
-   # Reading in spectra and doing interpolation if needed. For both methods, the wavebands used are based on very first OPUS spectra read-in. 
-   if(spectraInterp %in% c('stats_splinefun_lowess', 'prospectr_resample')) {     # Below simplify = FALSE, so interpolation is not done by Roudier's opusreader::opus_read(). 
-   
-      # ----------- Re-sampling wavebands using stats::splinefun() with lowess() (inside of the JRWToolBox::predict.lowess() function) or prospectr::resample() --------------
-      newScans.ADJ <- list()
-      for (i in fileNames.0)  {
-         print(i)
-         if(opusReader == 'pierreroudier_opusreader')
-            try(newScans.ADJ[[i]] <- opusreader::opus_read(paste(Spectra_Path, i , sep = "/"), simplify = TRUE, wns_digits = 0)[[2]] )
-            
-         if(opusReader == 'philippbaumann_opusreader2')
-            try(newScans.ADJ[[i]] <- opusreader2::read_opus_single(paste(Spectra_Path, i , sep = "/"))[[3]]$data ) 
-      }
-      
-      head(wavebandsNum <- as.numeric(colnames(newScans.ADJ[[1]])), 5)
-      # wavebandsNum <- trunc(wavebandsNum * 10)/10
-      # wavebandsToUse <- round(wavebandsNum)
-      # wavebandsToUse.tt <- ifelse(dec(wavebandsNum) > 0.5 - 0.00001 & dec(wavebandsNum) < 0.5 + 0.00001 & dec(trunc(wavebandsNum)/2) > 0 - 0.00001 & dec(trunc(wavebandsNum)/2) < 0 + 0.00001 , wavebandsToUse + 1, wavebandsToUse)
-      # 
-      # tmp <- cbind( wavebandsNum, decimal = wavebandsNum - trunc( wavebandsNum ), wavebandsToUse, wavebandsToUse.tt )
-      # tmp[tmp[,3] != tmp[,4], ]
-      # 
-      
-	  wavebandsToUse <- round(wavebandsNum)
-	  if(sum(wavebandsToUse - wavebandsNum) != 0) {
-         # For Sablefish 2017.2019 a handful of SG_Scan_Variables_only spectra (consecutive numbers) whose trunc() is an even number rounded up and not down when the decimal was 0.5. ????????????????
-         # c(length(SG_Scan_Variables_only), sum(as.numeric(substring(SG_Scan_Variables_only, 2)) %in% wavebandsToUse))
-         (adjNum <- as.numeric(substring(SG_Scan_Variables_only, 2))[!as.numeric(substring(SG_Scan_Variables_only, 2)) %in% wavebandsToUse])
-         
-         if(length(adjNum) >= 1) {
-           for (i in adjNum)
-             wavebandsToUse[wavebandsToUse == i - 1] <- i
-         }
-      }
-      # c(length(SG_Scan_Variables_only), sum(as.numeric(substring(SG_Scan_Variables_only, 2)) %in% wavebandsToUse))
-      
-      colnames(newScans.ADJ[[1]]) <- wavebandsToUse
-      
-      if(verbose & plot) {          # Extra adjustments added here - THIS IS EXPERIMENTAL
-         png(width = 16, height = 10, units = 'in', res = 600, file = paste0(Predicted_Ages_Path, '/Spline_Function_Raw.png'))
-         # plot(wavebandsToUse - 270, newScans.ADJ[[1]] + 0.10, type = 'l', ylim = c(0, 1.2), xlim = c(3500, 8000)) # For matching Sable 2022 to Sable 2019
-         plot(wavebandsToUse, newScans.ADJ[[1]], type = 'l', ylim = c(0, 1.2), xlim = c(3500, 8000))
-         
-         for(j in 2:length(fileNames)) {
-            wavebandsOld <- as.numeric(colnames(newScans.ADJ[[j]]))
-            adjFreq <- c(0, min(wavebandsToUse) - min(wavebandsOld) + fineFreqAdj)[1]
-            # adjFreq <- ifelse(j > 89, 0, -270) # For matching Sable 2022 to Sable 2019
-            adjAsorb <- c(0, mean(newScans.ADJ[[1]]) - mean(newScans.ADJ[[j]]))[1]
-            # adjAsorb <- ifelse(j > 89, 0, 0.10) # For matching Sable 2022 to Sable 2019
-            lines(wavebandsOld + adjFreq, newScans.ADJ[[j]] + adjAsorb, col = j)
-         }
-         abline(v = as.numeric(substring(SG_Scan_Variables_only, 2)), col = 'grey')
-         dev.off()
-         browseURL(paste0(getwd(), "/", Predicted_Ages_Path, '/Spline_Function_Raw.png'), browser = ifelse(.Platform$OS.type == 'windows', "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe", getOption("browser")))
-         # browseURL(paste0(getwd(), "/", Predicted_Ages_Path, '/Spline_Function_Raw.png'))
-      } 
-      
-      
-      # --  Create a dataframe (newScans.RAW) of scan results from the newScans.ADJ list, interpolating the results if the wavebands are not equal --
-      # newScans.ADJ[[1]] <- newScans.ADJ[[1]]
-      # c(length(SG_Scan_Variables_only), sum(as.numeric(substring(SG_Scan_Variables_only, 2)) %in% wavebandsToUse))
-      newScans.ADJ_int <- matrix(data = NA, nrow = length(newScans.ADJ), ncol = length(wavebandsToUse)) # Make empty matrix for loop
-      newScans.ADJ_int[1, ] <- newScans.ADJ[[1]]
-      
-      for (j in 2:length(newScans.ADJ)) { 
-        bar(j, length(newScans.ADJ))
-        wavebandsOld <- as.numeric(colnames(newScans.ADJ[[j]]))
-        if(all(wavebandsOld %in% wavebandsToUse))
-           newScans.ADJ_int[j,] <- newScans.ADJ[[j]]
-        else {
-           cat("\n\n*** Number", j, "waveband is not equal to the first waveband read-in ***\n\n")
-           adjFreq <- c(0, min(wavebandsToUse) - min(wavebandsOld) + fineFreqAdj)[1]
-           # print(c(j, adjFreq,  min(wavebandsToUse), min(wavebandsOld), min(wavebandsOld) + adjFreq))
-           adjAsorb <- c(0, mean(newScans.ADJ[[1]]) - mean(newScans.ADJ[[j]]))[1]
-           if(spectraInterp == 'stats_splinefun_lowess') 
-              newScans.ADJ_int[j,] <- predict.lowess(lowess(wavebandsOld + adjFreq, newScans.ADJ[[j]] + adjAsorb, f = 0.001), newdata = wavebandsToUse)
-           if(spectraInterp == 'prospectr_resample') 
-              newScans.ADJ_int[j,] <- prospectr::resample(X = newScans.ADJ[[j]] + adjAsorb, wav = wavebandsOld + adjFreq, new.wav = wavebandsToUse)   
-            
-           if(any(wavebandsToUse < min(wavebandsOld + adjFreq)))  # Hack for extrapolating beyond the end of the old wavebands.
-              newScans.ADJ_int[j,][wavebandsToUse < min(wavebandsOld + adjFreq)] <- newScans.ADJ_int[j,][wavebandsToUse < min(wavebandsOld + adjFreq)][1]
-           
-           # dev.new()   
-           # plot(wavebandsToUse, newScans.ADJ_int[j,], col = 'green')
-           # points(wavebandsOld + adjFreq, newScans.ADJ[[j]] + adjAsorb)
-                        
-        }
-      }
-      
-      # colnames(newScans.ADJ_int) <- colnames(newScans.ADJ[[1]])
-      colnames(newScans.ADJ_int) <- wavebandsToUse
-      # sum(as.numeric(substring(SG_Scan_Variables_only, 2)) %in% wavebandsToUse)
-      # newScans.RAW <- match.f(data.frame(fileNames, newScans.ADJ_int)[fileNames %in% Model_Spectra_Meta$filenames, ], Model_Spectra_Meta, 'fileNames', 'filenames', SG_Variables_Selected[metaDataVar])[, -1]
-	  newScans.RAW <- data.frame(newScans.ADJ_int)[fileNames %in% Model_Spectra_Meta$filenames, ]
-      # dim(newScans.RAW)
-	  # sum(apply(newScans.RAW, 2, function(x) sum(is.na(x)))) # Should be zero	 
+  
+   # Extract newScans.RAW, fileNames, and shortName from Model_Spectra_Meta
+   newScans.RAW <- Model_Spectra_Meta[, 2:(grep('project', names(Model_Spectra_Meta)) - 1)]
+   if(verbose) {
       print(head(newScans.RAW[, c(1:5, (ncol(newScans.RAW) - 5):(ncol(newScans.RAW)))])); cat("\n\n")
-	  shortName <- shortName[fileNames %in% Model_Spectra_Meta$filenames]
-	  fileNames <- fileNames[fileNames %in% Model_Spectra_Meta$filenames]
+      cat("\nDimension of Spectral File Matrix Read In:", dim(newScans.RAW), "\n\n")    
    }
-    
-  cat("\nDimension of Spectral File Matrix Read In:", dim(newScans.RAW), "\n\n")    
-      
+   
+   fileNames <- Model_Spectra_Meta$filenames
+   shortName <- apply(Model_Spectra_Meta[, 'filenames', drop = FALSE], 1, function(x) paste(get.subs(x, sep = "_")[c(1, 5)], collapse = "_"))
+  
+  
 # run <- function(...) {     # Use when debugging interactively to avoid error with dots (...)
    if(plot) {
      sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/FishNIRS/master/R/plotly.Spec.R")
@@ -285,7 +173,7 @@ Predict_NN_Age <- function(Conda_TF_Eniv, Spectra_Path, Model_Spectra_Meta, NN_M
   
    newScans <- match.f(data.frame(fileNames, newScans), Model_Spectra_Meta, 'fileNames', 'filenames', SG_Variables_Selected[metaDataVar])[, -1]  
    
-   save(newScans, newScans.RAW, file = 'newScans.RData')
+   # save(newScans, newScans.RAW, file = 'newScans.RData')
     
    if(is.null(NumRdmModels))
        N <- length(Rdm_models)
