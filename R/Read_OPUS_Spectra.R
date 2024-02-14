66603c7d0c506c1845e933cb16cc2e32068786fd
John Wallace <John.Wallace@noaa.gov>
2024-02-14 15:03:56 -0800

    adding length and weight from data warehouse

--- R/Read_OPUS_Spectra.R
+++ R/Read_OPUS_Spectra.R
@@ -51,14 +51,18 @@ Read_OPUS_Spectra <- function(Spectra_Set = c("Hake_2019", "Sable_2017_2019", "S
 	
 	# (4) Sablefish 2021, Combo survey
     if(Spectra_Set == "Sable_Combo_2021") { 
-       if(is.null(Spectra_Path)) 
-          Spectra_Path = 'C:/ALL_USR/JRW/SIDT/Sablefish 2021 Combo/Sable_2021_Scans'
-       if(Meta_Add & is.null(Meta_Path)) 
-          Meta_Path = paste0('C:/ALL_USR/JRW/SIDT/Sablefish 2021 Combo/', Spectra_Set, '_NIRS_Scanning_Session_Report.xlsx')
-       shortNameSegments <- c(1,5) # Segments 1 and 3 of the spectra file name, e.g.: (SABLEFISH, COMBO201701203A, 28, OD1) => (SABLEFISH, 28)
-       shortNameSuffix <- 'Combo'
-       yearPosition <- c(6, 9) # e.g. COMBO201701203A => 2017 (Segment used (see above) is: shortNameSegments[1] + 1)
-       fineFreqAdj <- 0
+        if(is.null(Spectra_Path)) 
+           Spectra_Path <- 'C:/ALL_USR/JRW/SIDT/Sablefish 2021 Combo/Sable_2021_Scans'
+        if(Meta_Add) {
+		    is.null(Meta_Path)
+                 Meta_Path <- paste0('C:/ALL_USR/JRW/SIDT/Sablefish 2021 Combo/', Spectra_Set, '_NIRS_Scanning_Session_Report.xlsx')
+		    load("C:/ALL_USR/JRW/SIDT/Sablefish/Sable_Combo_Ages_DW.RData")  # 'DW' is NWFSC Data Warehouse
+		    metadata_DW <- Sable_Combo_Ages_DW; rm(Sable_Combo_Ages_DW)
+	    }	  
+        shortNameSegments <- c(1,5) # Segments 1 and 3 of the spectra file name, e.g.: (SABLEFISH, COMBO201701203A, 28, OD1) => (SABLEFISH, 28)
+        shortNameSuffix <- 'Combo'
+        yearPosition <- c(6, 9) # e.g. COMBO201701203A => 2017 (Segment used (see above) is: shortNameSegments[1] + 1)
+        fineFreqAdj <- 0
     }  
 # -----------------------------------------------------------------------------------------------------------------------------------
  
@@ -207,31 +211,33 @@ Read_OPUS_Spectra <- function(Spectra_Set = c("Hake_2019", "Sable_2017_2019", "S
        cat("\nDimension of Spectral File Matrix Read In:", dim(newScans.RAW), "\n\n")
        
     if(Meta_Add) {
-       fileNames <- get.subs(fileNames.0, sep = ".")[1, ]  # No '.0' in the metadata xlsx
-       metadata <- openxlsx::read.xlsx(Meta_Path, sheet = excelSheet) # Load in ancillary data 
-       Model_Spectra_Meta <- dplyr::left_join(data.frame(filenames = fileNames, newScans.RAW), metadata, dplyr::join_by("filenames" == "NWFSC_NIR_Filename")) # Match by filenames and look at the data/metadata
-       if(verbose)
-          print(Model_Spectra_Meta[1:5, c(1:3, (ncol(Model_Spectra_Meta) - 36):ncol(Model_Spectra_Meta))])
-       Model_Spectra_Meta$length_prop_max <- Model_Spectra_Meta$Length_cm/max(Model_Spectra_Meta$Length_cm, na.rm = TRUE)
-       Model_Spectra_Meta$structure_weight_dg = 10 * Model_Spectra_Meta$structure_weight_g # dg = decigram
-       Model_Spectra_Meta$percent_crystallized_scan[is.na(Model_Spectra_Meta$percent_crystallized_scan)] <- 0 # Change NA to zero so that a numerical test can be done.
-       Model_Spectra_Meta$percent_missing_scan[is.na(Model_Spectra_Meta$percent_missing_scan)] <- 0
-       Model_Spectra_Meta$tissue_level_scan[is.na(Model_Spectra_Meta$tissue_level_scan)] <- 0
-       Model_Spectra_Meta <- data.frame(Model_Spectra_Meta, shortName = shortName) 
-       
-       TF <- Model_Spectra_Meta$percent_crystallized_scan <= 15 & Model_Spectra_Meta$percent_crystallized_scan <= 10 & Model_Spectra_Meta$tissue_level_scan <= 10 & 
-                     !is.na(Model_Spectra_Meta$Length_cm) & !is.na(Model_Spectra_Meta$structure_weight_g)
-       names(Model_Spectra_Meta)[names(Model_Spectra_Meta) %in% 'age_best'] <- "TMA"    
-       if(TMA_Ages)        
-           TF <- TF & !is.na(Model_Spectra_Meta$TMA)
-       Model_Spectra_Meta <- Model_Spectra_Meta[TF, ]
-       
-       if(verbose)
-          cat(paste0('\n\nTotal number of oties read in: ', sum(TF) + sum(!TF), '.  Number rejected based on metadata (including missing TMA, when asked for): ', sum(!TF), '.  Number kept: ', sum(TF), '.\n'), quote = FALSE)
-       
-       invisible(Model_Spectra_Meta) 
+        fileNames <- get.subs(fileNames.0, sep = ".")[1, ]  # No '.0' in the metadata xlsx
+        metadata <- openxlsx::read.xlsx(Meta_Path, sheet = excelSheet) # Load in ancillary data 
+        Model_Spectra_Meta <- dplyr::left_join(data.frame(filenames = fileNames, newScans.RAW), metadata, dplyr::join_by("filenames" == "NWFSC_NIR_Filename")) # Match by filenames and look at the data/metadata
+        metadata$length_cm <- metadata$weight_kg <- NULL   
+	    metadata <- match.f(metadata, metadata_DW, "specimen_id", "AgeStr_id", c('Length_cm', 'Weight_kg'))
+        if(verbose)
+           print(Model_Spectra_Meta[1:5, c(1:3, (ncol(Model_Spectra_Meta) - 36):ncol(Model_Spectra_Meta))])
+        Model_Spectra_Meta$length_prop_max <- Model_Spectra_Meta$Length_cm/max(Model_Spectra_Meta$Length_cm, na.rm = TRUE)
+        Model_Spectra_Meta$structure_weight_dg = 10 * Model_Spectra_Meta$structure_weight_g # dg = decigram
+        Model_Spectra_Meta$percent_crystallized_scan[is.na(Model_Spectra_Meta$percent_crystallized_scan)] <- 0 # Change NA to zero so that a numerical test can be done.
+        Model_Spectra_Meta$percent_missing_scan[is.na(Model_Spectra_Meta$percent_missing_scan)] <- 0
+        Model_Spectra_Meta$tissue_level_scan[is.na(Model_Spectra_Meta$tissue_level_scan)] <- 0
+        Model_Spectra_Meta <- data.frame(Model_Spectra_Meta, shortName = shortName) 
+        
+        TF <- Model_Spectra_Meta$percent_crystallized_scan <= 15 & Model_Spectra_Meta$percent_crystallized_scan <= 10 & Model_Spectra_Meta$tissue_level_scan <= 10 & 
+                      !is.na(Model_Spectra_Meta$Length_cm) & !is.na(Model_Spectra_Meta$structure_weight_g)
+        names(Model_Spectra_Meta)[names(Model_Spectra_Meta) %in% 'age_best'] <- "TMA"    
+        if(TMA_Ages)        
+            TF <- TF & !is.na(Model_Spectra_Meta$TMA)
+        Model_Spectra_Meta <- Model_Spectra_Meta[TF, ]
+        
+        if(verbose)
+           cat(paste0('\n\nTotal number of oties read in: ', sum(TF) + sum(!TF), '.  Number rejected based on metadata (including missing TMA, when asked for): ', sum(!TF), '.  Number kept: ', sum(TF), '.\n'), quote = FALSE)
+        
+        invisible(Model_Spectra_Meta) 
     } else  
-      invisible(newScans.RAW)
+        invisible(newScans.RAW)
 }   
       
      
