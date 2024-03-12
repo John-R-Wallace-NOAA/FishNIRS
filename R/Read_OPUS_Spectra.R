e7737a801684fe29a4350da9a49f76e612766f2a
John Wallace <John.Wallace@noaa.gov>
2024-03-11 20:30:20 -0700

    Various fixes

--- R/Read_OPUS_Spectra.R
+++ R/Read_OPUS_Spectra.R
@@ -236,39 +236,52 @@ Read_OPUS_Spectra <- function(Spectra_Set = c("Hake_2019", "Sable_2017_2019", "S
         fileNames <- get.subs(fileNames.0, sep = ".")[1, ]  # No '.0' in the metadata xlsx
         metadata <- openxlsx::read.xlsx(Meta_Path, sheet = excelSheet) # Load in ancillary data 
         Model_Spectra_Meta <- dplyr::left_join(data.frame(filenames = fileNames, newScans.RAW), metadata, dplyr::join_by("filenames" == "NWFSC_NIR_Filename")) # Match by filenames and look at the data/metadata
+		names(Model_Spectra_Meta)[names(Model_Spectra_Meta) %in% 'age_best'] <- "TMA"
+		Model_Spectra_Meta$percent_crystallized_scan[is.na(Model_Spectra_Meta$percent_crystallized_scan)] <- 0 # Change NA to zero so that a numerical test can be done.
+        Model_Spectra_Meta$percent_missing_scan[is.na(Model_Spectra_Meta$percent_missing_scan)] <- 0  # Change NA to zero so that a numerical test can be done.
+		Model_Spectra_Meta$tissue_level_scan[is.na(Model_Spectra_Meta$tissue_level_scan)] <- 'none' # Change NA to 'none' tissue level.
+		Model_Spectra_Meta <- data.frame(Model_Spectra_Meta, shortName = shortName) 
+		
         Model_Spectra_Meta$length_cm <- Model_Spectra_Meta$weight_kg <- NULL   
-	    Model_Spectra_Meta <- match.f(Model_Spectra_Meta, metadata_DW, "specimen_id", "AgeStr_id", c('Length_cm', 'Weight_kg', 'Month_Scaled', 'Depth_m', 'Sex', 'Days_into_Year')) 
-        if(!is.null(Model_Spectra_Meta$Length_cm))		
-           Model_Spectra_Meta$length_prop_max <- Model_Spectra_Meta$Length_cm/max(Model_Spectra_Meta$Length_cm, na.rm = TRUE)
+		
+	    Model_Spectra_Meta <- match.f(Model_Spectra_Meta, metadata_DW, "specimen_id", "AgeStr_id", c('Length_cm', 'Weight_kg', 'Sex', 'Depth_m', 'Month', 'Days_into_Year'))
+		
 		if(!is.null(Model_Spectra_Meta$structure_weight_g))	   
            Model_Spectra_Meta$structure_weight_dg = 10 * Model_Spectra_Meta$structure_weight_g # dg = decigram
+		   
+        if(!is.null(Model_Spectra_Meta$Length_cm))		
+           Model_Spectra_Meta$Length_prop_max <- Model_Spectra_Meta$Length_cm/max(Model_Spectra_Meta$Length_cm, na.rm = TRUE)
+		   
+		if(!is.null(Model_Spectra_Meta$Weight_kg))
+           Model_Spectra_Meta$Weight_prop_max <- (Model_Spectra_Meta$Weight_kg - min(Model_Spectra_Meta$Weight_kg, na.rm = TRUE))/(max(Model_Spectra_Meta$Weight_kg, na.rm = TRUE) - min(Model_Spectra_Meta$Weight_kg, na.rm = TRUE))
+		   
         if(!is.null(Model_Spectra_Meta$Sex))
            Model_Spectra_Meta$Sex_prop_max <- as.numeric(recode.simple(Model_Spectra_Meta$Sex, data.frame(c('F','M', 'U'), 0:2)))/2  # ** All variables have to be numeric ** 
+		   
         if(!is.null(Model_Spectra_Meta$Depth_m))
            Model_Spectra_Meta$Depth_prop_max <- (Model_Spectra_Meta$Depth_m - min(Model_Spectra_Meta$Depth_m, na.rm = TRUE))/(max(Model_Spectra_Meta$Depth_m, na.rm = TRUE) - min(Model_Spectra_Meta$Depth_m, na.rm = TRUE))
-        if(!is.null(Model_Spectra_Meta$Weight_kg))
-           Model_Spectra_Meta$Weight_prop_max <- (Model_Spectra_Meta$Weight_kg - min(Model_Spectra_Meta$Weight_kg, na.rm = TRUE))/(max(Model_Spectra_Meta$Weight_kg, na.rm = TRUE) - min(Model_Spectra_Meta$Weight_kg, na.rm = TRUE))
-        if(!is.null(Model_Spectra_Meta$Days_into_Year_prop_max))
-           Model_Spectra_Meta$Days_into_Year <- (Model_Spectra_Meta$Days_into_Year - min(Model_Spectra_Meta$Days_into_Year, na.rm = TRUE))/(max(Model_Spectra_Meta$Days_into_Year, na.rm = TRUE) - min(Model_Spectra_Meta$Days_into_Year, na.rm = TRUE))
-        Model_Spectra_Meta$percent_crystallized_scan[is.na(Model_Spectra_Meta$percent_crystallized_scan)] <- 0 # Change NA to zero so that a numerical test can be done.
-        Model_Spectra_Meta$percent_missing_scan[is.na(Model_Spectra_Meta$percent_missing_scan)] <- 0
-        Model_Spectra_Meta <- data.frame(Model_Spectra_Meta, shortName = shortName) 
-        
-		# For consistennce, don't use the 'light' (or other level) of tissue level until a study is done
-        # TF <- Model_Spectra_Meta$percent_crystallized_scan <= 15 & Model_Spectra_Meta$percent_crystallized_scan <= 10 & !is.na(Model_Spectra_Meta$Month_Scaled) & 
+		   
+		if(!is.null(Model_Spectra_Meta$Month))   
+		   Model_Spectra_Met$Month_Scaled <- Model_Spectra_Met$Month/12
+		   
+        if(!is.null(Model_Spectra_Meta$Days_into_Year))
+           Model_Spectra_Meta$Days_into_Year_prop_max <- (Model_Spectra_Meta$Days_into_Year - min(Model_Spectra_Meta$Days_into_Year, na.rm = TRUE))/(max(Model_Spectra_Meta$Days_into_Year, na.rm = TRUE) - min(Model_Spectra_Meta$Days_into_Year, na.rm = TRUE))
+		   
+       	# For consistennce, don't use the 'light' (or other level) of tissue level until a study is done
+        # TF <- Model_Spectra_Meta$percent_crystallized_scan <= 15 & Model_Spectra_Meta$percent_missing_scan <= 10 & !is.na(Model_Spectra_Meta$Month_Scaled) & 
 		#         (Model_Spectra_Meta$tissue_level_scan == "light" | is.na(Model_Spectra_Meta$tissue_level_scan)) & !is.na(Model_Spectra_Meta$Length_cm) & !is.na(Model_Spectra_Meta$structure_weight_g)
 				  
-        TF <- Model_Spectra_Meta$percent_crystallized_scan <= 15 & Model_Spectra_Meta$percent_crystallized_scan <= 10 & is.na(Model_Spectra_Meta$tissue_level_scan) & 
-		          !is.na(Model_Spectra_Meta$Length_cm) & !is.na(Model_Spectra_Meta$structure_weight_g) & !is.na(Model_Spectra_Meta$Month_Scaled)
-				  
-        names(Model_Spectra_Meta)[names(Model_Spectra_Meta) %in% 'age_best'] <- "TMA"    
+        TF <- Model_Spectra_Meta$percent_crystallized_scan <= 15 & Model_Spectra_Meta$percent_missing_scan <= 10 & Model_Spectra_Meta$tissue_level_scan == 'none' 
+		
         if(TMA_Ages)        
             TF <- TF & !is.na(Model_Spectra_Meta$TMA)
+			
         Model_Spectra_Meta <- renum(Model_Spectra_Meta[TF, ])
         
         if(verbose) {
 		   print(Model_Spectra_Meta[1:3, c(1, (grep('project', names(Model_Spectra_Meta))):ncol(Model_Spectra_Meta))])
            cat(paste0('\n\nTotal number of oties read in: ', sum(TF) + sum(!TF), '.  Number rejected based on metadata (including missing TMA, when asked for): ', sum(!TF), '.  Number kept: ', sum(TF), '.\n'), quote = FALSE)
+		   cat("After the particular metadata is selected for a model run, remove those oties which contain any missing values.\n\n")
          }
 		 
         invisible(Model_Spectra_Meta)
