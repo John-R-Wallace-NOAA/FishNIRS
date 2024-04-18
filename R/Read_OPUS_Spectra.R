916a465cadf0f38a3524b0d255c246a759295721
John Wallace <John.Wallace@noaa.gov>
2024-04-18 14:32:43 -0700

    removed ALL_USR/JRW

--- R/Read_OPUS_Spectra.R
+++ R/Read_OPUS_Spectra.R
@@ -19,7 +19,7 @@ Read_OPUS_Spectra <- function(Spectra_Set = c("Hake_2019", "Sable_2017_2019", "S
     # Hake 2019, BMS
     if(Spectra_Set == "Hake_2019") {
        if(interactive())
-          setwd("C:/ALL_USR/JRW/SIDT/Predict_NN_Ages")  # Change path to the Spectra Set's .GlobalEnv as needed
+          setwd("C:/SIDT/Predict_NN_Ages")  # Change path to the Spectra Set's .GlobalEnv as needed
        shortNameSegments <- c(2, 4) # Segments 2 and 4 of the spectra file name, e.g.: (PACIFIC, HAKE, BMS201906206C, 1191, OD1) => (HAKE, 1191)
        shortNameSuffix <- 'BMS'
        opusReader <- 'pierreroudier_opusreader'
@@ -29,7 +29,7 @@ Read_OPUS_Spectra <- function(Spectra_Set = c("Hake_2019", "Sable_2017_2019", "S
     # Sablefish 2017 & 2019, Combo survey
     if(Spectra_Set == "Sable_2017_2019") { 
        if(interactive()) 
-          setwd("C:/ALL_USR/JRW/SIDT/Predict_NN_Ages")  
+          setwd("C:/SIDT/Predict_NN_Ages")  
        shortNameSegments <- c(1, 3) # Segments 1 and 3 of the spectra file name, e.g.: (SABLEFISH, COMBO201701203A, 28, OD1) => (SABLEFISH, 28)
        shortNameSuffix <- 'Year'
        yearPosition <- c(6, 9) # e.g. COMBO201701203A => 2017 (Segment used (see above) is: shortNameSegments[1] + 1)
@@ -41,11 +41,11 @@ Read_OPUS_Spectra <- function(Spectra_Set = c("Hake_2019", "Sable_2017_2019", "S
     if(Spectra_Set %in% c("Sable_Combo_2017", "Sable_Combo_2018", "Sable_Combo_2019", "Sable_Combo_2021", "Sable_Combo_2022")) { 
 	   Year <- get.subs(Spectra_Set, sep = "_")[3]
        if(is.null(Spectra_Path)) 
-          Spectra_Path <- paste0('C:/ALL_USR/JRW/SIDT/Sablefish ', Year, ' Combo/Sable_Combo_', Year, '_Scans')
+          Spectra_Path <- paste0('C:/SIDT/Sablefish ', Year, ' Combo/Sable_Combo_', Year, '_Scans')
        if(Meta_Add) {
 		    is.null(Meta_Path)
-                 Meta_Path <- paste0('C:/ALL_USR/JRW/SIDT/Sablefish ', Year, ' Combo/', Spectra_Set, '_NIRS_Scanning_Session_Report_For_NWFSC.xlsx')
-		    base::load("C:/ALL_USR/JRW/SIDT/Get Otie Info from Data Warehouse/selectSpAgesFramFeb2024.RData")  
+                 Meta_Path <- paste0('C:/SIDT/Sablefish ', Year, ' Combo/', Spectra_Set, '_NIRS_Scanning_Session_Report_For_NWFSC.xlsx')
+		    base::load("C:/SIDT/Get Otie Info from Data Warehouse/selectSpAgesFramFeb2024.RData")  
 		    metadata_DW <- selectSpAgesFramFeb2024; rm(selectSpAgesFramFeb2024) # 'DW' is NWFSC Data Warehouse
 	    }	  
        shortNameSegments <- c(1,5) # Segments 1 and 3 of the spectra file name, e.g.: (SABLEFISH, COMBO201701203A, 28, OD1) => (SABLEFISH, 28)
