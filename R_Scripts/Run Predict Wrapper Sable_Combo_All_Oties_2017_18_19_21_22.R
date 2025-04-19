b3b7d00bc60b79c178c9e817cc27a69b085692ef
John Wallace <John.Wallace@noaa.gov>
2025-04-18 17:19:49 -0700

    Add files via upload

--- R_Scripts/Run Predict Wrapper Sable_Combo_All_Oties_2017_18_19_21_22.R
+++ R_Scripts/Run Predict Wrapper Sable_Combo_All_Oties_2017_18_19_21_22.R
@@ -20,7 +20,7 @@ sourceFunctionURL <- function (URL,  type = c("function", "script")[1]) {
 }
 
 
-# sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/FishNIRS/master/R_Scripts/Predict_NN_Age_Wrapper.R")
+# sourceFunctionURL("https://raw.githubusercontent.com/John-R-Wallace-NOAA/FishNIRS/master/R/Predict_NN_Age_Wrapper.R")
  
  
 # source("C:/SIDT/Train_NN_Model/Read_OPUS_Spectra.R")
@@ -108,47 +108,68 @@ Predict_NN_Age_Wrapper(Spectra_Set = Spectra_Set, Train_Result_Path = "C:/SIDT/S
       Metadata_Extra_File = "C:/SIDT/Sablefish Combo Multi Year/Sable_Combo_Multi_15__24_Model_Spectra_Meta_ALL_GOOD_DATA.RData", Delta_Given = list(-0.15, NULL)[[2]],
   Model_Spectra_Meta_Path = "C:/SIDT/Sablefish Combo Multi Year/Sable_Combo_Multi_15__24_Model_Spectra_Meta_ALL_GOOD_DATA.RData", main = "Metadata: Str Wgt, Len, Wgt, Depth, Lat") 
 	
-
-
  
- # For debug and the figure below
+ # For debug 
  Graph_Metadata = c("structure_weight_g", "Length_cm", "Weight_kg", "Depth_m", "Latitude_dd", "Sex", "Month"); F_vonBert = list(Linf = 64, k = 0.32, t0 = 0); M_vonBert = list(Linf = 57, k = 0.41, t0 = 0)
  Multi_Year = TRUE; TMA_Ages_Only = FALSE; TMA_Ages = TRUE
  Metadata_Extra = c("structure_weight_g", "Length_cm", "Weight_kg", "Depth_m", "Latitude_dd", "Sex", "Month"); Metadata_Extra_File = "C:/SIDT/Sablefish Combo Multi Year/Sable_Combo_Multi_15__24_Model_Spectra_Meta_ALL_GOOD_DATA.RData"  
  Meta_Data_Factors = c("Sex", "Month")
- 
+  
  
 
+# --- Fix up New_Ages for sharing ---
+load("C:\\SIDT\\Sablefish Combo Multi Year\\Sable_Combo_All_Oties_2015__2024\\Predicted_Ages_No_Bias_Corr\\NN Predicted Ages, 17 Apr 2025.RData")
+New_Ages$Used_NN_Model[is.na(New_Ages$Used_NN_Model)] <- FALSE 
+New_Ages$structure_weight_dg <- New_Ages$Index <- New_Ages$Length_prop_max <- New_Ages$Weight_prop_max <- New_Ages$Depth_prop_max <- New_Ages$Latitude_prop_max <- NULL
+names(New_Ages)[grep('Year', names(New_Ages))] <- "Year_Project"
+names(New_Ages)[grep('Pred_Age_Bias_Corr_plus_Delta_rounded', names(New_Ages))] <- "Pred_Age_Corr_plus_Delta_rounded"
+names(New_Ages)[grep('Pred_Age_Bias_Corr_plus_Delta_rounded_Minus_TMA', names(New_Ages))] <- "Pred_Age_Corr_plus_Delta_rounded_Minus_TMA"
+sum(Table(New_Ages$Year_Project, New_Ages$Used_NN_Model))
+headTail(New_Ages)
+save(New_Ages, file = "C:\\SIDT\\Sablefish Combo Multi Year\\Sable_Combo_All_Oties_2015__2024\\Predicted_Ages_No_Bias_Corr\\NN Predicted Ages, 17 Apr 2025.RData")
 
+
+ 
 
 
  
 # --- Bias correction with Kelli's 2016 von Bert with fixed T0 at zero thru 2024 ---
 Predict_NN_Age_Wrapper(Spectra_Set = Spectra_Set, Train_Result_Path = "C:/SIDT/Sablefish Combo Multi Year/Sable_Combo_All_Oties_2017_18_19_21_22", Multi_Year = TRUE, Use_Session_Report_Meta = FALSE, 
-  Bias_Adj_Factor_Ages = c(23, 30:70), Bias_Reduction_Factor = 0.8, Lowess_smooth_para = 2/3, TMA_Ages = TRUE, TMA_Ages_Only = FALSE, # TMA_Ages_Only = FALSE => Predictions without TMA
+  Bias_Adj_Factor_Ages = c(56, 30:70), Bias_Reduction_Factor = 0.8, Lowess_smooth_para = 2/3, TMA_Ages = TRUE, TMA_Ages_Only = FALSE, # TMA_Ages_Only = FALSE => Predictions without TMA
   # Metadata_Extra = c("Ship", "Cruise", "Haul", "Date_UTC", "Length_cm", "Sex", "Weight_kg", "Net_Partition"), 
   Metadata_Extra = c("structure_weight_g", "Length_cm", "Weight_kg", "Depth_m", "Latitude_dd", "Sex", "Month"), Meta_Data_Factors = c("Sex", "Month"), scanUniqueName = 'filenames', Debug_plotly.Spec = TRUE,
   Graph_Metadata = c("structure_weight_g", "Length_cm", "Weight_kg", "Depth_m", "Latitude_dd", "Sex", "Month"), F_vonBert = list(Linf = 64, k = 0.32, t0 = 0), M_vonBert = list(Linf = 57, k = 0.41, t0 = 0),
-      Metadata_Extra_File = "C:/SIDT/Sablefish Combo Multi Year/Sable_Combo_Multi_15__24_Model_Spectra_Meta_ALL_GOOD_DATA.RData", Delta_Given = list(-0.15, NULL)[[2]],
+      Metadata_Extra_File = "C:/SIDT/Sablefish Combo Multi Year/Sable_Combo_Multi_15__24_Model_Spectra_Meta_ALL_GOOD_DATA.RData", Delta_Given = list(0, NULL)[[1]],
   Model_Spectra_Meta_Path = "C:/SIDT/Sablefish Combo Multi Year/Sable_Combo_Multi_15__24_Model_Spectra_Meta_ALL_GOOD_DATA.RData", main = "Metadata: Str Wgt, Len, Wgt, Depth, Lat") 
 	
 
-    
-# To try
 
-Bias_Adj_Factor_Ages = c(23, 30:70), Bias_Reduction_Factor = 0.8      
-
-
+ # For debug 
+ Graph_Metadata = c("structure_weight_g", "Length_cm", "Weight_kg", "Depth_m", "Latitude_dd", "Sex", "Month"); F_vonBert = list(Linf = 64, k = 0.32, t0 = 0); M_vonBert = list(Linf = 57, k = 0.41, t0 = 0)
+ Multi_Year = TRUE; TMA_Ages_Only = FALSE; TMA_Ages = TRUE
+ Metadata_Extra = c("structure_weight_g", "Length_cm", "Weight_kg", "Depth_m", "Latitude_dd", "Sex", "Month"); Metadata_Extra_File = "C:/SIDT/Sablefish Combo Multi Year/Sable_Combo_Multi_15__24_Model_Spectra_Meta_ALL_GOOD_DATA.RData"  
+ Meta_Data_Factors = c("Sex", "Month")
+  
+  
 # Tried 
 Bias_Adj_Factor_Ages = c(7, 8:70), Bias_Reduction_Factor = 1.25  # Huge gap :-/
+Bias_Adj_Factor_Ages = c(23, 30:70), Bias_Reduction_Factor = 0.8 # small gap
+Bias_Adj_Factor_Ages = c(7, 30:70), Bias_Reduction_Factor = 0.8,  # gap
+Bias_Adj_Factor_Ages = c(0, 30:70), Bias_Reduction_Factor = 0.8 # screws up zero
 
 
+# --- Fix up New_Ages for sharing ---
+load("C:\\SIDT\\Sablefish Combo Multi Year\\Sable_Combo_All_Oties_2015__2024\\Predicted_Ages_Bias_Corr\\NN Predicted Ages, 18 Apr 2025.RData")
+New_Ages$Used_NN_Model[is.na(New_Ages$Used_NN_Model)] <- FALSE 
+New_Ages$structure_weight_dg <- New_Ages$Index <- New_Ages$Length_prop_max <- New_Ages$Weight_prop_max <- New_Ages$Depth_prop_max <- New_Ages$Latitude_prop_max <- NULL
+names(New_Ages)[grep('Year', names(New_Ages))] <- "Year_Project"
+sum(Table(New_Ages$Year_Project, New_Ages$Used_NN_Model))
+sum(Table(New_Ages$Year_Project, New_Ages$Bias_Adj))
+headTail(New_Ages)
+save(New_Ages, file = "C:\\SIDT\\Sablefish Combo Multi Year\\Sable_Combo_All_Oties_2015__2024\\Predicted_Ages_Bias_Corr\\NN Predicted Ages, 18 Apr 2025.RData")
 
 
-
-
-
-
+ 
 
 
 
